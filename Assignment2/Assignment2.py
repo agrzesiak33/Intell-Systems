0643dae6ed5633087b1e71229a255416b1c41690
Alex Grzesiak <agrzesiak@hotmail.com>
2017-02-13 22:08:44 -0700

    Adding 3 new dictionaries to help with the eventual heuristic function
    -dictPrefix has been deleted
    -dict(numWordsWithPrefix) has been added to replace set(dictPrefix)
    -dict(averageWordScore) has been added
    -dict(avgNumLettersAfterPrefix) has been added 
    -Made handling the 'qu' case better

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -1,4 +1,4 @@
-import math
+import time
 from collections import deque
 
 class Scroggle(object):
@@ -8,8 +8,10 @@ class Scroggle(object):
         self.frontier=[]
         self.board=[]
         self.numQueries=0
-        self.dictPrefix = set()
         self.dict=set()
+        self.numWordsWithPrefix = {}
+        self.averageWordScore = {}
+        self.avgNumLettersAfterPrefix = {}
         self.letterWeights = []
         self.goodWords = set()
 
@@ -27,38 +29,79 @@ class Scroggle(object):
     #I handle the qu case by adding the word with adn without the u in the word.
     #As per the instructions, a board with the word 'qeen' should be translated as 'queen'.
     def handleQU(self, word):
-        tempString = ""
+        stringWithU = ""
+        stringWithoutU = ""
         for letter in word:
-            if tempString=="":
-                tempString+=letter
-            elif tempString[-1]=="q" and letter=="u":
+            if stringWithU=="":
+                stringWithU+=letter
+                stringWithoutU += letter
+            elif stringWithU[-1]=="q" and letter=="u":
+                stringWithU += letter
                 continue
             else:
-                tempString+=letter
+                stringWithoutU+=letter
+            self.addTo_numWordsWithPrefix(stringWithU)
+            self.addTo_numWordsWithPrefix(stringWithoutU)
 
-        for i in range(len(tempString) - 1):
-            if i == (len(tempString) - 2):
-                self.dict.add(tempString)
-                self.dictPrefix.add(tempString)
-            else:
-                self.dictPrefix.add(tempString[:i + 2])
+    def addTo_numWordsWithPrefix(self, word):
+        try:
+            self.numWordsWithPrefix[word] += 1
+        except KeyError:
+            self.numWordsWithPrefix[word] = 1
+
 
     def importDictionary(self, dictFile):
+        startTime = time.perf_counter()
         lastLine=""
+        prefix = ""
+        wordScore = 0
+        oldAverage = 0
+        oldNumPrefixes = 0
+        newAverage = 0
         with open(dictFile) as f:
             for word in f:
                 word = word[:-1]
                 lastline=word
+                wordScore = self.getWordScore(word)
+
                 if "qu" in word:
                     self.handleQU(word)
-                for i in range(len(word)-1):
-                    if i==(len(word)-2):
+
+                for i in range(len(word)):
+                    if i==(len(word)-1):
                         self.dict.add(word)
-                        self.dictPrefix.add(word)
+                        try:
+                            self.numWordsWithPrefix[word] += 1
+                        except KeyError:
+                            self.numWordsWithPrefix[word] = 1
                     else:
-                        self.dictPrefix.add(word[:i + 2])
+                        prefix = word[:i + 1]
+                        #   If the prefix has already been encounteres
+                        try:
+                            oldAverage = self.averageWordScore[prefix]
+                            oldNumPrefixes = self.numWordsWithPrefix[prefix]
+                            newAverage = (oldAverage + wordScore)/(oldNumPrefixes+1)
+                            self.averageWordScore[prefix] = newAverage
+                            self.numWordsWithPrefix[prefix] += 1
+
+                            oldAverage = self.avgNumLettersAfterPrefix[prefix]
+                            newAverage = (oldAverage + len(word))/(oldNumPrefixes+1)
+                            self.avgNumLettersAfterPrefix[prefix] = newAverage
+                        #   If the prefix is new
+                        except KeyError:
+                            self.numWordsWithPrefix[prefix] = 1
+                            self.averageWordScore[prefix] = wordScore
+                            self.avgNumLettersAfterPrefix[prefix] = len(word)
         self.dict.add(lastline)
-        self.dictPrefix.add(lastline)
+        self.addTo_numWordsWithPrefix(lastline)
+        print("Time to import dictionaries: ", time.perf_counter() - startTime)
+
+
+    def getWordScore(self, word):
+        wordScore = 0
+        for char in word:
+            wordScore += self.letterWeights[ord(char) - ord('a')]
+        return wordScore
 
     def importWeights(self, weightPath):
         with open(weightPath) as f:
@@ -85,6 +128,9 @@ class Scroggle(object):
                     availableMoves . append([x,y])
         return availableMoves
 
+    def heuristic1(self):
+        return
+
 
     # @brief    Searches the board for goal states
     # @details  Depending on the searchType, it usees BFS, DFS, or A* to
@@ -92,46 +138,74 @@ class Scroggle(object):
     #
     # @param[in]    searchType
     #               0 = DFS     1 = BFS     2 = A*
-    def scroggle(self, searchType):
-        #if self.numQueries==0:
-         #   return
+    # @param[in[    queryLimit
+    #               integer of the number of dictionary queries the solver is allowed
+    #               queryLimit < 0 = unlimited      querLimit > 0 = finite
+    # $param[in]    dumbness
+    #               True for no prefix optimization     False for optimization
+    def scroggle(self, searchType, queryLimit, dumbness):
+        if(queryLimit == 0):
+            return -1
+        self.numQueries = queryLimit
 
-        currentPath=[]
-        if searchType ==0:
-            currentPath=self.frontier.pop()
-        elif searchType==1:
-          currentPath=self.frontier.popLeft()
-
+        dictQueries =0
+        startTime = time.perf_counter()
+        newWord=""
+        newPath = []
+        newScore = 0
         foundPartial = False
-        if len(currentPath[0]) > 1:
-            self.numQueries-=1
-            if (currentPath[0] in self.dictPrefix):
-                foundPartial = True
-                if currentPath[0] in self.dict:
-                    self.goodWords.add(currentPath[0])
 
-        if(foundPartial == True or len(currentPath[0])==1):
-            availablePaths=self.findPossiblePaths(currentPath[1])
-            if availablePaths == []:
+        while self.frontier:
+            if self.numQueries==0 and queryLimit > 0:
                 return
 
-            newWord=""
-            newPath = list(currentPath[1])
-            newScore = 0
-            for availablePath in availablePaths:
-                newWord = currentPath[0] + self.board[(self.dimen * availablePath[0]) + availablePath[1]]
-                newPath.append(availablePath)
-                newScore = currentPath[2] + self.letterWeights[ ord(newWord[-1]) - ord('a') ]
-                self.frontier.append([newWord, newPath, newScore ])
-                newPath=list(currentPath[1])
+            currentPath=[]
+            if searchType ==0:
+                currentPath=self.frontier.pop()
+            elif searchType==1:
+              currentPath=self.frontier.popleft()
 
-            self.scroggle(searchType)
+
+
+
+
+
+            #   Check to see if word is in the dictionary
+            foundPartial = False
+            if len(currentPath[0]) > 1:
+                self.numQueries-=1
+                dictQueries+=1
+                try:
+                    self.numWordsWithPrefix[currentPath[0]]
+                    foundPartial = True
+                    if(currentPath[0] in self.dict):
+                        self.goodWords.add(currentPath[0])
+                except KeyError:
+                    foundPartial = False
+
+            #   Expand nodes and add them to the frontier
+            if(foundPartial == True or len(currentPath[0])==1 or dumbness == True):
+                availablePaths=self.findPossiblePaths(currentPath[1])
+                if availablePaths == []:
+                    continue
+
+                newWord=""
+                newPath = list(currentPath[1])
+                newScore = 0
+                for availablePath in availablePaths:
+                    newWord = currentPath[0] + self.board[(self.dimen * availablePath[0]) + availablePath[1]]
+                    newPath.append(availablePath)
+                    newScore = currentPath[2] + self.letterWeights[ ord(newWord[-1]) - ord('a') ]
+                    self.frontier.append([newWord, newPath, newScore ])
+                    newPath=list(currentPath[1])
+        print("Solver runtime: ", time.perf_counter()-startTime)
+        print("Dict queries: ", dictQueries)
 
 
 
 scroggleInstance = Scroggle()
 scroggleInstance.importWeights("scrabble-vals.txt")
-scroggleInstance.importBoard("twoboard.txt")
+scroggleInstance.importBoard("fourboard2.txt")
 scroggleInstance.importDictionary("dict.txt")
-scroggleInstance.scroggle(0)
-print(scroggleInstance.goodWords)
+scroggleInstance.scroggle(0,-1,False)
+print(scroggleInstance.numWordsWithPrefix["az"])
